1538b9118c9e4532e1502b2e57ecf36c8f0c60cb
Paulo Campos <paulo.devcampos@gmail.com>
2023-04-16 15:52:45 -0300

    projeto final

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+let validaEmail = () => {
+    let email = document.getElementById("emailInput").value;
+    let emailValido = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email);
+
+    if(!emailValido){
+        document.getElementById("msgEmail").innerHTML = 'Email inválido';
+    }else{
+        document.getElementById("msgEmail").innerHTML = '';
+    }
+    return emailValido;
+}
+
+let validaTelefone = () => {
+    let telefone = document.getElementById("telefoneInput").value; 
+    let telefoneValido =  /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/.test(telefone);
+
+    if(!telefoneValido){
+        document.getElementById("msgTel").innerHTML = 'Telefone inválido';
+    }else{
+        document.getElementById("msgTel").innerHTML = '';
+    }
+    return telefoneValido;
+}
+
+let validaNome = () => {
+    let nomeValido = document.getElementById("nomeInput").value != ''; 
+
+    if(!nomeValido){
+        document.getElementById("msgNome").innerHTML = 'Preencha o nome';
+    }else{
+        document.getElementById("msgNome").innerHTML = '';
+    }
+
+}
+
+let confirmaCadastro = (telefoneValido, emailValido) => {
+    if(telefoneValido && emailValido){
+        alert("Cadastreido meu asmuigo!");
+    }
+}
+
+function validaCampos(){
+    
+    let telefoneValido = validaTelefone();   
+    let emailValido = validaEmail();
+    validaNome();
+    confirmaCadastro(telefoneValido, emailValido);
+}
