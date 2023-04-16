8ca5c750a62caa20aa941da0c63a2c872d3eb6e0
Paulo Campos <paulo.devcampos@gmail.com>
2023-04-16 20:08:27 -0300

    menu hamburguer ajustes

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@ let menu = () => {
     if(menuid.style.display == 'block'){
         menuid.style.display = 'none'
         navid.style.background = '#B90504'
+        spanid.style.color = 'white'
     }else{
         menuid.style.display = 'block'
         navid.style.background = 'white'
